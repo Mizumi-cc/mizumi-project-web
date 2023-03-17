de6372dd6ee36be3235d7c77f6c86cacc42d626b
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2023-03-17 01:21:18 +0000

    HTMLHead component

---
+++ components/HTMLHead.tsx
@@ -0,0 +1,133 @@
+import { NextSeo } from "next-seo"
+import { MetaTag } from "next-seo/lib/types"
+import Head from "next/head"
+
+interface HTMLHeadProps {
+  title: string
+  description: string
+  meta?: ReadonlyArray<MetaTag>
+  socialShareImage?: string
+  socialShareUrl?: string
+  addDefaultMeta?: boolean
+}
+
+const HTMLHead = ({
+  title = '',
+  description = '',
+  meta = [],
+  socialShareImage = '',
+  socialShareUrl = '',
+  addDefaultMeta = true,
+}: HTMLHeadProps) => {
+  const metaTitle = title ? title : 'Mizumi Inc.'
+  const metaDescription = description ? description : 'Mizumi Inc.'
+  const metaSocialShareImage = socialShareImage ? socialShareImage : 'https://mizumi.cc/img/mizumi-text.png'
+  const metaTwitter = 'https://twitter.com/MizumiInc'
+  const metaSocialShareUrl = socialShareUrl ? socialShareUrl : metaTwitter
+
+  const defaultMeta: ReadonlyArray<MetaTag> = [
+    {
+      name: 'description',
+      content: metaDescription,
+    },
+    {
+      property: 'og:title',
+      content: metaTitle,
+    },
+    {
+      property: 'og:description',
+      content: metaDescription,
+    },
+    {
+      property: 'og:image',
+      content: metaSocialShareImage,
+    },
+    {
+      property: 'og:url',
+      content: metaSocialShareUrl,
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:image',
+      content: metaSocialShareImage,
+    },
+    {
+      name: 'twitter:title',
+      content: metaTitle,
+    },
+    {
+      name: 'twitter:site',
+      content: metaTwitter,
+    },
+    {
+      name: 'twitter:url',
+      content: metaSocialShareUrl,
+    },
+    {
+      name: 'twitter:description',
+      content: metaDescription,
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1',
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: '#ffffff',
+    },
+    {
+      name: 'theme-color',
+      content: '#ffffff',
+    },
+    {
+      name: 'keywords',
+      content: 'Mizumi, Inc., Mizumi, Mizumi Inc, Mizumi Inc., Mizumi Inc, Mizumi Inc., stablecoin - fiat swaps, decentralized, crypto, Solana, Solana blockchain, Africa'
+    }
+    ...meta
+  ]
+
+  return [
+    <>
+      <NextSeo 
+        {...(addDefaultMeta && { metaTitle })}
+        titleTemplate={`%s | ${metaTitle}`}
+        additionalMetaTags={addDefaultMeta ? defaultMeta : meta}
+      />
+      <Head>
+        <title>{metaTitle}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
+    </>
+  ]
+}
+
+export default HTMLHead
