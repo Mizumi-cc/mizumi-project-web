ba96ffa85f872caab5bedb3494570d134312dead
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2023-05-11 20:43:13 +0000

    fix storybook installation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,22 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  "stories": ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  "typescript": {
+    reactDocgen: false
+  },
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+    });
+
+    return config;
+  },
+};
