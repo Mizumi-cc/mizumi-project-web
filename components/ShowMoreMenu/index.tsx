91b0be7acbcaeff62f4a1dddfb45f9786b42fbb0
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2023-06-07 00:02:19 +0000

    change ShowMoreMenu background color

--- components/ShowMoreMenu/index.tsx
+++ components/ShowMoreMenu/index.tsx
@@ -31,16 +31,14 @@ const ShowMoreMenu: FunctionComponent<Props> = ({ options }) => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute right-0 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-black shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               {options.map((option, optionIdx) => (
                 <Menu.Item key={optionIdx}>
                   {({ active }) => (
                     <button
                       onClick={option.onClick}
-                      className={`${
-                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      className="group flex w-full items-center rounded-md px-2 py-2 text-sm text-white font-medium"
                     >
                       {option.title}
                     </button>
