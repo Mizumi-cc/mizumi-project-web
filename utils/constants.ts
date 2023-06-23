0a534850b019764a784e34ac5395ce1e6184cc76
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2023-06-23 14:07:13 +0000

    fix lint

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,5 @@
 export const CURRENCIES = [
   { name: 'Ghana Cedi', symbol: 'GHS', image: '/assets/img/cedi.png' },
   { name: 'USDC', symbol: 'USDC', image: '/assets/img/usdc.svg', mintAddress: process.env.NEXT_PUBLIC_USDC_MINT },
-  { name: 'USDT', symbol: 'USDT', image: 'https://raw.githubusercontent.com/      solana-labs/token-list/main/assets/mainnet/ 2n4pLBdDtciUsEyR2WoS2cTgDiyTHysVQF4caHttV44v/logo.png', mintAddress: process.env.  NEXT_PUBLIC_USDT_MINT 
-  },
+  { name: 'USDT', symbol: 'USDT', image: 'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/2n4pLBdDtciUsEyR2WoS2cTgDiyTHysVQF4caHttV44v/logo.png', mintAddress: process.env.NEXT_PUBLIC_USDT_MINT },
 ]
-
